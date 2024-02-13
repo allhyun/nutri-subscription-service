904bdc3c6dd08139dfb450ec510f13c466d6072e
allhyun <da0956@naver.com>
2024-02-13 14:56:12 +0900

    first commit

---
+++ src/main/java/project3/nutrisubscriptionservice/NutriSubscriptionServiceApplication.java
@@ -0,0 +1,13 @@
+package project3.nutrisubscriptionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriSubscriptionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutriSubscriptionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project3/nutrisubscriptionservice/NutriSubscriptionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package project3.nutrisubscriptionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutriSubscriptionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
