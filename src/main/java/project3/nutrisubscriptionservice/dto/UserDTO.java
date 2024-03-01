9694ef1677e902334b31efcd289cda4610f55866
allhyun <da0956@naver.com>
2024-03-01 22:37:56 +0900

    feat/order...

--- src/main/java/project3/nutrisubscriptionservice/dto/UserDTO.java
+++ src/main/java/project3/nutrisubscriptionservice/dto/UserDTO.java
@@ -1,13 +1,18 @@
 package project3.nutrisubscriptionservice.dto;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import project3.nutrisubscriptionservice.entity.UserEntity;
 
 @Getter
 @Builder
 //@NoArgsConstructor
+@AllArgsConstructor
+
 public class UserDTO {
 
     private long id;
@@ -17,5 +22,18 @@ public class UserDTO {
     private String phone;
     private String zipcode;
     private String address;
+
+    public UserDTO(UserEntity userEntity) {
+        this.id=userEntity.getId();
+        this.email=userEntity.getEmail();
+        this.password=userEntity.getPassword();
+        this.name=userEntity.getName();
+        this.phone=userEntity.getPhone();
+        this.zipcode=userEntity.getZipcode();
+        this.address=userEntity.getAddress();
+
+    }
+
+
 }
 
