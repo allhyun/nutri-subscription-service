93dbd6f018290368ca5031794edd7cca3d87bc1c
allhyun <da0956@naver.com>
2024-03-08 09:51:08 +0900

    set/cart order & orderList

--- src/main/java/project3/nutrisubscriptionservice/controller/OrderListController.java
+++ src/main/java/project3/nutrisubscriptionservice/controller/OrderListController.java
@@ -8,30 +8,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 import project3.nutrisubscriptionservice.dto.*;
-import project3.nutrisubscriptionservice.entity.OrderEntity;
-import project3.nutrisubscriptionservice.entity.OrderItemEntity;
-import project3.nutrisubscriptionservice.entity.OrderListEntity;
-import project3.nutrisubscriptionservice.entity.UserEntity;
 import project3.nutrisubscriptionservice.repository.OrderListRepository;
 import project3.nutrisubscriptionservice.repository.UserRepository;
 import project3.nutrisubscriptionservice.service.CartService;
 import project3.nutrisubscriptionservice.service.OrderItemService;
-import project3.nutrisubscriptionservice.service.OrderService;
 import project3.nutrisubscriptionservice.service.UserService;
 
 import java.security.Principal;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/orderliness")
@@ -67,61 +57,11 @@ public class OrderListController {
             return ResponseEntity.ok(OrderList);
 
     }
-
-
-    // 사용자 ID 별 주문 조회
-//    @GetMapping("/{userId}")
-//    public ResponseEntity<List<OrderListEntity>> findByMyUserId(@PathVariable Long userId) {
-//        try {
-//            UserEntity user = userRepository.findById(userId)
-//                    .orElseThrow(() -> new RuntimeException("User not found for this id :: " + userId));
-//
-//            List<OrderListEntity> orderHistory = orderListRepository.findByUser(user);
-//
-//            return new ResponseEntity<>(orderHistory, HttpStatus.OK);
-//        } catch (Exception e) {
-//            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-//        }
-//    }
-//    public ResponseEntity<?> findByMyUserId(@PathVariable Long userId) {
-//        try {
-//            UserEntity user = userRepository.findById(userId)
-//                    .orElseThrow(() -> new RuntimeException("User not found for this id :: " + userId));
-//
-//            List<OrderListEntity> orderHistory = orderListRepository.findByUser(user);
-//
-//            OrderListDTO orderHistoryDtoList = (OrderListDTO) orderHistory.stream()
-//                    .map(order->{return new OrderListDTO(order);})
-//                    .collect(Collectors.toList());
-//
-//            OrderListDTO orderListDTO = OrderListDTO.builder()
-//                    .orderlistId(orderHistoryDtoList.getOrderlistId())
-//                    .id(orderHistoryDtoList.getId())
-//                    .orderItems(orderHistoryDtoList.getOrderItems())
-//                    .orderdate(orderHistoryDtoList.getOrderdate())
-//                    .build();
-//
-//            return ResponseEntity.ok().body(orderListDTO);
-//        } catch (Exception e) {
-//            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-//        }
-//    }
     // 단일 상품 주문
     @PreAuthorize("isAuthenticated()")
     @PostMapping(value = "/order/add")
     @ResponseBody
-    public ResponseEntity order(@RequestBody @Valid OrderListDTO orderDto,
-                                BindingResult bindingResult, Principal principal) {
-
-        if (bindingResult.hasErrors()) {
-            StringBuilder sb = new StringBuilder();
-            List<FieldError> fieldErrors = bindingResult.getFieldErrors();
-            for (FieldError fieldError : fieldErrors) {
-                sb.append(fieldError.getDefaultMessage());
-            }
-            return new ResponseEntity<String>(sb.toString(), HttpStatus.BAD_REQUEST);
-        }
-
+    public ResponseEntity order(@RequestBody @Valid OrderListDTO orderDto, Principal principal) {
         Long orderId;
         log.error("eeee {} ",  principal.getName());
         try {
@@ -130,47 +70,23 @@ public class OrderListController {
             log.error("Exception:", e);
             return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
-        return new ResponseEntity<Long>(orderId, HttpStatus.OK);
+        return ResponseEntity.ok().body("주문이 완료되었습니다");
     }
-
-
-   @PostMapping(value = "/cart/orders")
-   public ResponseEntity<Long> placeOrder(@RequestBody List<OrderListDTO> orderDtoList, Long userId, @AuthenticationPrincipal UserDetails userDetails) {
-       try {
-           // 현재 로그인한 사용자의 이메일 가져오기
-           String email = userDetails.getUsername();
-
-           // 주문을 생성하고 주문 ID를 가져옴
-           Long orderId = orderItemService.orders(orderDtoList, userId);
-
-//           // 주문 아이템 목록을 이용하여 주문 생성
-//           Long userId = orderItemService.orderFromCart(orderDtoList, userId);
-
-           return ResponseEntity.ok(orderId);
-       } catch (Exception e) {
-           return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
-       }
-   }
-
-    @DeleteMapping("/{userId}/remove")
-    public ResponseEntity<Void> removeOrderItem(@PathVariable Long userId, @RequestParam Long productId,Long orderlistid) {
-        orderItemService.removeOrderItem(userId, productId,orderlistid);
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
+    //장바구니 상품 주문
     @PostMapping("/cart/orderlist")
     @ResponseBody
-    public ResponseEntity ordersFromCart(@RequestBody List<CartDTO> cartDTO, Principal principal) {
+    public ResponseEntity ordersFromCart(@RequestBody  List<CartDTO> cartDTO, Principal principal) {
+
+        Long orderId;
         try {
+
             // 현재 로그인한 사용자의 이메일 가져오기
             String email = principal.getName();
-
-
             // 장바구니 주문을 처리하고 주문 ID를 가져옴
-            Long orderId = orderItemService.orderCartItem(cartDTO, email);
+            orderId = orderItemService.orderCartItem(cartDTO,email);
             log.info("email : {}" , orderItemService.orderCartItem(cartDTO, email));
 
-            return ResponseEntity.ok(orderId);
+
         } catch (EntityNotFoundException e) {
             // 장바구니나 사용자를 찾을 수 없는 경우
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("장바구니나 사용자를 찾을 수 없습니다.");
@@ -178,15 +94,14 @@ public class OrderListController {
             // 예상치 못한 예외가 발생한 경우
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("주문 처리 중에 오류가 발생했습니다.");
         }
+        return ResponseEntity.ok().body("장바구니 주문이 완료되었습니다");
+   }
+
+    @DeleteMapping("/{userId}/remove")
+    public ResponseEntity<Void> removeOrderItem(@PathVariable Long userId, @RequestParam Long productId,Long orderlistid) {
+        orderItemService.removeOrderItem(userId, productId,orderlistid);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
-
-
-
-
-
-
-
-
 
 
 }
--- src/main/java/project3/nutrisubscriptionservice/service/OrderItemService.java
+++ src/main/java/project3/nutrisubscriptionservice/service/OrderItemService.java
@@ -1,16 +1,14 @@
 package project3.nutrisubscriptionservice.service;
 
-import io.micrometer.core.instrument.Meter;
+
 import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.weaver.ast.Or;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import project3.nutrisubscriptionservice.dto.*;
@@ -21,7 +19,6 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -83,7 +80,6 @@ public class OrderItemService {
         for (OrderItemDTO orderItemDTO : orderItemDTOList) {
             settingProductDTO(orderItemDTO);
         }
-
         // OrderListDTO 객체 생성 및 설정
         // OrderListDTO 빌더 패턴 사용 (주의: 여기에 있는 `.id`는 UserEntity의 ID를 설정하는 것이 아닐 수 있음)
         return OrderListDTO.builder()
@@ -97,8 +93,6 @@ public class OrderItemService {
 
 //////////////////////////////////////////////////////////////////////////////////
 
-
-    ///////////////////////////////////////////////??//////
     //orderListDTO&Entity
     //DTO
     public void addOrderItemDto(OrderItemDTO orderItemDTO){
@@ -121,23 +115,10 @@ public class OrderItemService {
     public static OrderListEntity createOrder(UserEntity user,List<OrderItemEntity> orderItemEntities){
         OrderListEntity order = new OrderListEntity();
         order.setUser(user);
-//        for(OrderItemEntity orderItem : orderItemEntities){
-//            addOrderItemList(orderItem);
 //        }
         order.setOrderItemEntitiy(orderItemEntities);
         order.setOrderdate(LocalDateTime.now());
         return order;
-    }
-
-
-    public static int getTotalPrice(){
-        int totalPrice=0;
-        List<OrderItemEntity> orderItemEntities = new ArrayList<>();
-        for(OrderItemEntity orderItemEntity : orderItemEntities){
-            totalPrice += getTotalPrice();
-
-        }return totalPrice;
-
     }
     //orderItemDTO&Entity
     //Entity
@@ -146,12 +127,12 @@ public class OrderItemService {
         OrderItemEntity orderItem = new OrderItemEntity();
         orderItem.setProduct(product);
         orderItem.setCount(count);
-        orderItem.setOrderPrice(product.getPPrice());
+        orderItem.setOrderPrice(product.getPPrice()*count);
 
         return orderItem;
     }
 
-    public int getTotaitemPrice() {
+    public static int getTotaitemPrice() {
         OrderItemEntity orderItem = new OrderItemEntity();
 
         return orderItem.getOrderPrice()*orderItem.getCount();
@@ -203,26 +184,6 @@ public class OrderItemService {
 //        orderCancle();
 //    }
 
-    //장바구니 상품주문
-    public Long orders(List<OrderListDTO> orderDtoList, Long userId ) {
-
-        // 로그인한 유저 조회
-        UserEntity user = userRepository.findById(Long.valueOf(userId)).orElseThrow();
-
-        // orderDto 객체를 이용하여 item 객체와 count 값을 얻어낸 뒤, 이를 이용하여 OrderItem 객체(들) 생성
-        List<OrderItemEntity> orderItem = new ArrayList<>();
-        for (OrderListDTO orderDto : orderDtoList) {
-            ProductEntity product = productRepository.findById(orderItem.get(0).getProduct().getProductId()).orElseThrow(EntityNotFoundException::new);
-            OrderItemEntity orderItemEntity = createOrderItem(product, orderItem.get(0).getCount());
-            orderItem.add(orderItemEntity);
-        }
-
-        //Order Entity 클래스에 존재하는 createOrder 메소드로 Order 생성 및 저장
-        OrderListEntity order = createOrder(user, orderItem);
-        orderListRepository.save(order);
-        return order.getUser().getId();
-    }
-
     // 장바구니 상품(들) 주문
     public Long orderCartItem(List<CartDTO> cartDTO, String userId) {
 
@@ -238,56 +199,42 @@ public class OrderItemService {
                 OrderItemEntity orderItem = new OrderItemEntity();
                 // 필요한 정보를 설정 (예: 상품, 수량 등)
                 orderItem.setProduct(cartProduct.getProduct());
+                orderItem.setOrderPrice(cartProduct.getProduct().getPPrice()*cartProduct.getQuantity());
                 orderItem.setCount(cartProduct.getQuantity());
                 // 생성된 주문 항목을 리스트에 추가
                 orderItems.add(orderItem);
             }
-
             // 장바구니에서 상품을 주문한 후 장바구니 삭제
             cartRepository.delete(cartEntity);
         }
 
         // 주문 생성 및 저장
         UserEntity user = userRepository.findById(Long.valueOf(userId)).orElseThrow();
-        OrderListEntity order = createOrder(user, orderItems);
-        orderListRepository.save(order);
-
-        // 주문한 사용자의 ID 반환
-        return order.getUser().getId();
 
 
+        OrderListEntity orderListEntity =  createOrder(user,orderItems);
+        // orderListEntity에 id 설정
+        orderListEntity.setUser(user); // user 객체로 설정
+        log.info("id 2 {}",orderListEntity.getUser().getId());
 
+        // orderListEntity에 orderdate와 id 설정
+        orderListEntity.setOrderdate(LocalDateTime.now()); // 현재 날짜와 시간으로 설정 혹은 필요에 따라 다른 방법 사용
 
+        // OrderList 객체 DB 저장 (Cascade로 인해 OrderItem 객체도 같이 저장)
+        orderListRepository.save(orderListEntity);
 
+        // OrderListEntity의 id 값 가져오기
+        Long orderListId = orderListEntity.getOrderlistId();
 
+//        Long cartId = cartEntity.getCartId();
+        // OrderItemEntity에 orderlist_id 설정 및 저장
+        for (OrderItemEntity orderItemss : orderItems) {
+            orderItemss.setOrderList(orderListEntity); // OrderItemEntity의 orderlist_id 설정
+            orderItemRepository.save(orderItemss); // OrderItemEntity 저장
+        }
+        return orderListId;
 
-
-//        List<OrderItemEntity> orderlistDto = new ArrayList<>();
-//
-//        for (CartDTO cartDto : cartDTO) {
-//            CartEntity cartt = cartRepository.findById(cartDto.getCartId()).orElseThrow(EntityNotFoundException::new);
-//            OrderListDTO orderDto = new OrderListDTO();
-//            orderDto.setId(cartt.getUser().getId());
-//            orderlistDto.add(orderDto);
-//        }
-//        log.info("eeeee : {}", orderlistDto);
-//        // OrderList 객체 생성
-//
-//        //UserEntity user = userRepository.findById(Long.valueOf(userId)).orElseThrow();
-//        //OrderListEntity orderListEntity =  createOrder(user, orderItemList);
-//
-//        Long orderId = orders(orderlistDto, Long.valueOf(userId));
-//        log.info("eeeee : {},{}", orderlistDto, Long.valueOf(userId));
-//        // 주문한 장바구니 상품을 제거
-//        for (CartDTO cartDto : cartDTO) {
-//            CartEntity cartt = cartRepository.findById(cartDto.getCartId()).orElseThrow(EntityNotFoundException::new);
-//            cartRepository.delete(cartt);
-//        }
-//        return orderId;
     }
-
-
-
     ///////////////////////////////////////////////////////////////////////////////
     public void  settingProductDTO(OrderItemDTO orderItemDTO){
         log.info("product id {}", orderItemDTO.getProducts());
