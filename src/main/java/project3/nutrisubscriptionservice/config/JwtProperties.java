76869ff1e8f00e9eef06ff30b90f947402393b02
allhyun <da0956@naver.com>
2024-03-18 22:15:24 +0900

    feat/jwt

---
+++ src/main/java/project3/nutrisubscriptionservice/config/JwtProperties.java
@@ -0,0 +1,16 @@
+package project3.nutrisubscriptionservice.config;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Setter
+@Getter
+@Component
+@ConfigurationProperties("jwt")
+public class JwtProperties {
+    private String issuer;
+    private String secretKey;
+}
